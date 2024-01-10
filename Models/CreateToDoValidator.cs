4adcde52a9b8e0871111289d6e121f9bc80a279c
Jens Jonsson <jens.jonsson@bitsdata.se>
2024-01-10 13:39:35 +0100

    WIP

---
+++ Models/CreateToDoValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace BindingDemo.Models;
+
+public class CreateToDoValidator : AbstractValidator<CreateToDoModel>
+{
+    public CreateToDoValidator()
+    {
+        RuleFor(x => x.Description).NotEmpty();
+        RuleFor(x => x.Description).MinimumLength(5);
+    }
+}
---
+++ Models/CreateTodoModel.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation.Results;
+
+namespace BindingDemo.Models;
+
+public class CreateToDoModel
+{
+    // Should be bound
+    public string Description { get; set; }
+
+    // Should NOT be bound
+    public List<ValidationFailure>? Errors { get; set; }
+}
